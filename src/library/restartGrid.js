58dd50a0d214975f131c0824b501f9cc459069e7
Vitor <vitorascorrea@gmail.com>
2021-04-19 08:49:47 -0300

    Restart grid function now receives a dynamic grid size value

--- src/library/restartGrid.js
+++ src/library/restartGrid.js
@@ -1,7 +1,5 @@
-export const restartGrid = () => {
-  return [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ];
+export const restartGrid = (grid_size = 3) => {
+  const grid = new Array(grid_size).fill(new Array(grid_size).fill(''));
+
+  return grid;
 };
